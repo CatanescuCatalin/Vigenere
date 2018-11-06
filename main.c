9d870ef1a9644dfa97514aa28bdeb5e15881c0cb
Catanescu Catalin <catanescucatalin@yahoo.com>
2018-11-06 19:56:23 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,55 @@
+//
+//  main.c
+//  Vigenere
+//
+//  Created by MacBook on 06/11/2018.
+//  Copyright © 2018 MacBook. All rights reserved.
+//
+
+
+#include<stdio.h>
+#include<string.h>
+
+int Vigenere(char msg[],char key[]){
+
+    int msgLen = strlen(msg), keyLen = strlen(key), i, j;
+    
+    char newKey[msgLen], encryptedMsg[msgLen], decryptedMsg[msgLen];
+    
+    //generating new key
+    for(i = 0, j = 0; i < msgLen; ++i, ++j){
+        if(j == keyLen)
+            j = 0;
+        
+        newKey[i] = key[j];
+    }
+    
+    newKey[i] = '\0';
+    
+    //encryption
+    for(i = 0; i < msgLen; ++i)
+        encryptedMsg[i] = ((msg[i] + newKey[i]) % 26) + 'A';
+    
+    encryptedMsg[i] = '\0';
+    
+    //decryption
+    for(i = 0; i < msgLen; ++i)
+        decryptedMsg[i] = (((encryptedMsg[i] - newKey[i]) + 26) % 26) + 'A';
+    
+    decryptedMsg[i] = '\0';
+    
+    printf("Original Message: %s", msg);
+    printf("\nKey: %s", key);
+    printf("\nNew Generated Key: %s", newKey);
+    printf("\nEncrypted Message: %s", encryptedMsg);
+    printf("\nDecrypted Message: %s\n", decryptedMsg);
+    
+    return 0;
+}
+
+int main()
+{
+    Vigenere("GETUPORGIVEUP","FIGHT");
+
+    return 0;
+}
